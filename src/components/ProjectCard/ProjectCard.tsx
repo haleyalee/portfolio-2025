34b47a42dfcab1f2332df7a73bbb9e7775289aaf
Haley <haleyalee@gmail.com>
2025-01-19 20:09:48 -0500

    add images and urls

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styles from "./ProjectCard.module.scss";
+import Image from 'next/image';
+import GithubIcon from '../Icons/Github';
+import ExternalIcon from '../Icons/External';
 
 interface ProjectCardProps {
   title: string,
   description: string,
   techStack: string[],
-  url?: string
+  url?: string,
+  github?: string,
+  img: string
 }
-const ProjectCard = ({title = "Project Title", description = "Description", techStack = ["TechStack", "TechStack", "TechStack"]}: ProjectCardProps) => {
+const ProjectCard = ({
+  title = "Project Title", 
+  description = "Description", 
+  techStack = ["TechStack", "TechStack", "TechStack"], 
+  url,
+  github, 
+  img
+}: ProjectCardProps) => {
   return (
     <div className={styles.card}>
+      <div className={styles.image}>
+        <Image 
+          src={img} 
+          alt={title} 
+          fill 
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
+          unoptimized
+        />
+      </div>
       <div className={styles.description}>
         <div className={styles.details}>
           <h3>{title}</h3>
@@ -19,6 +40,10 @@ const ProjectCard = ({title = "Project Title", description = "Description", tech
           { techStack.map((tech, i) => 
             <div key={i}>{tech}</div>
           )}
+        </div>
+        <div className={styles.urls}>
+          { url && <a href={url}><ExternalIcon /></a>}
+          { github && <a href={github}><GithubIcon /></a>}
         </div>
       </div>
     </div>
--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -10,31 +10,41 @@ const myWork = [
     title: "Recipes",
     description: "website to save and search personal recipes",
     techStack: ["React", "Next.js", "TypeScript", "Tailwind CSS", "PostgreSQL"],
-    url: ""
+    url: "",
+    github: "https://github.com/haleyalee/recipes",
+    img: "/recipes.gif"
   },
   {
     title: "Radiant Design System UI Library",
     description: "private web component UI library used internally at Discover",
-    techStack: ["React", "TypeScript", "MaterialUI"],
-    url: ""
+    techStack: ["React", "TypeScript", "MaterialUI", "Storybook"],
+    url: "https://labs.discover.com/storybook/next/?path=/docs/getting-started-intro--docs",
+    github: "",
+    img: "/discover.png"
   },
   {
     title: "Notion API Expense Tracker",
-    description: "data visualization tool for manually tracked expenses in Notion",
+    description: "a quick data visualization tool for manually tracked expenses in Notion",
     techStack: ["React", "TypeScript", "Express"],
-    url: ""
+    url: "",
+    github: "https://github.com/haleyalee/expenses-visualization",
+    img: "/notion-expenses.gif"
   },
   {
     title: "Plantome",
     description: "ecommerce website to buy plants",
     techStack: ["React", "TypeScript", "Bootstrap", "AWS"],
-    url: ""
+    url: "",
+    github: "https://github.com/haleyalee/plantome",
+    img: "/plantome.gif"
   },
   {
     title: "Max Portfolio",
     description: "personal website for Maxwell Lee",
     techStack: ["HTML/CSS", "JavaScript"],
-    url: ""
+    url: "https://haleyalee.github.io/max-website/",
+    github: "https://github.com/haleyalee/max-website",
+    img: "/max.webp"
   },
 ];
 
@@ -57,6 +67,8 @@ const Work = () => {
             description={proj.description}
             techStack={proj.techStack}
             url={proj.url}
+            github={proj.github}
+            img={proj.img}
           />
         </motion.div>
       )}
