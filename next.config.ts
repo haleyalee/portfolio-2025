0b6f5e405ec5483ab20f9243ca8fb8e8f554f76b
Haley <haleyalee@gmail.com>
2025-01-19 20:42:40 -0500

    build config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
